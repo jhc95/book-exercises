2502f9aaf0256890a3a8550dc13be008f964bb30
jhc95 <narsineke@gmail.com>
2020-01-30 15:27:47 -0800

    week 4 exercises

--- chapter-11-exercises/exercise-1/exercise.R
+++ chapter-11-exercises/exercise-1/exercise.R
@@ -11,38 +11,41 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- unique(vehicles$make)
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+length(makes)
 
 # Filter the data set for vehicles manufactured in 1997
-
+vehicles[vehicles['year'] == 1997,]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
+f <- vehicles[order(vehicles$hwy),]
+f[f$year == 1997,]
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+f['average'] <- (f$hwy + f$cty) / 2
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+f2 <- vehicles[vehicles$drive == "2-Wheel Drive",]
+f3 <- f2[f2$cty > 20,]
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+worst <- min(f3$hwy)
+w <- f3[f3$hwy == worst, 'id']
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
--- chapter-11-exercises/exercise-2/exercise.R
+++ chapter-11-exercises/exercise-2/exercise.R
@@ -8,15 +8,15 @@
 library(fueleconomy)
 
 # Install and load the "dplyr" library
-
+library("dplyr")
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- select(vehicles, make)
 
 # Use the `distinct()` function to determine how many different car manufacturers
 # are represented by the data set
-
+lengths(distinct(makes))
 
 # Filter the data set for vehicles manufactured in 1997
 
